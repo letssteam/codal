1e79cc51bd965f7f7ab1e6e6c78924c78373e623
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2020-06-30 13:41:26 +0300

    Add configurable sample program

--- source/main.cpp
+++ source/main.cpp
@@ -1,30 +1,23 @@
-#include "wiring_digital.h"
-#include "wiring_time.h"
-#include "variant.h"
-#include "board.h"
+#include "STM32IotNode.h"
 
-// Force init to be called *first*, i.e. before static object allocation.
-// Otherwise, statically allocated objects that need HAL may fail.
-__attribute__((constructor(101))) void premain()
-{
-  init();
-}
+#define TIMER_SAMPLE
+
+#if defined(GPIO_SAMPLE)
+    #include "GPIOSample.h"
+#elif defined(ADC_SAMPLE)
+    #include "ADCSample.h"
+#elif defined(SERIAL_SAMPLE)
+    #include "SerialSample.h"
+#elif defined(TIMER_SAMPLE)
+    #include "TimerSample.h"
+#else
+    #include "BlinkSample.h"
+#endif
+
+using namespace codal;
 
 int main()
 {
-    pinMode(LED1, OUTPUT);
-    pinMode(LED2, OUTPUT);
-    int pin_state = 0;
-
-    while (1) {
-        pin_state = !pin_state;
-        if (pin_state) {
-            digitalWrite(LED1, HIGH);
-            digitalWrite(LED2, LOW);
-        } else {
-            digitalWrite(LED1, LOW);
-            digitalWrite(LED2, HIGH);
-        }
-        delay(500);
-    }
+    SAMPLE_MAIN();
+    release_fiber();
 }
